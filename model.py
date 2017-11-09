181a0d33968c3a7e3231001efd7401cb6d97b423
彭冲 <pengchong@meituan.com>
2017-11-09 16:56:54 +0800

    in detect function,length of images must be equal to BATCH_SIZE

--- model.py
+++ model.py
@@ -2198,6 +2198,7 @@ class MaskRCNN():
         masks: [H, W, N] instance binary masks
         """
         assert self.mode == "inference", "Create model in inference mode."
+        assert len(images) == self.config.BATCH_SIZE, "len(images) must be equal to BATCH_SIZE"
 
         if verbose: 
             log("Processing {} images".format(len(images)))
