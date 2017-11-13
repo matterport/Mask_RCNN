a51a157fc7528151d098408ae9013e946b780d56
Waleed Abdulla <waleed.abdulla@gmail.com>
2017-11-13 05:38:32 -0800

    Refactor out overlap_graph() function

--- model.py
+++ model.py
@@ -419,6 +419,35 @@ class PyramidROIAlign(KE.Layer):
 #  Detection Target Layer
 ############################################################
 
+def overlaps_graph(boxes1, boxes2):
+    """Computes IoU overlaps between two sets of boxes.
+    boxes1, boxes2: [N, (y1, x1, y2, x2)].
+    """
+    # 1. Tile boxes2 and repeate boxes1. This allows us to compare
+    # every boxes1 against every boxes2 without loops.
+    # TF doesn't have an equivalent to np.repeate() so simulate it
+    # using tf.tile() and tf.reshape.
+    b1 = tf.reshape(tf.tile(tf.expand_dims(boxes1, 1),
+                            [1, 1, tf.shape(boxes2)[0]]), [-1, 4])
+    b2 = tf.tile(boxes2, [tf.shape(boxes1)[0], 1])
+    # 2. Compute intersections
+    b1_y1, b1_x1, b1_y2, b1_x2 = tf.split(b1, 4, axis=1)
+    b2_y1, b2_x1, b2_y2, b2_x2 = tf.split(b2, 4, axis=1)
+    y1 = tf.maximum(b1_y1, b2_y1)
+    x1 = tf.maximum(b1_x1, b2_x1)
+    y2 = tf.minimum(b1_y2, b2_y2)
+    x2 = tf.minimum(b1_x2, b2_x2)
+    intersection = tf.maximum(x2 - x1, 0) * tf.maximum(y2 - y1, 0)
+    # 3. Compute unions
+    b1_area = (b1_y2 - b1_y1) * (b1_x2 - b1_x1)
+    b2_area = (b2_y2 - b2_y1) * (b2_x2 - b2_x1)
+    union = b1_area + b2_area - intersection
+    # 4. Compute IoU and reshape to [boxes1, boxes2]
+    iou = intersection / union
+    overlaps = tf.reshape(iou, [tf.shape(boxes1)[0], tf.shape(boxes2)[0]])
+    return overlaps
+
+
 def detection_targets_graph(proposals, gt_class_ids, gt_boxes, gt_masks, config):
     """Generates detection targets for one image. Subsamples proposals and
     generates target class IDs, bounding box deltas, and masks for each.
@@ -459,28 +488,7 @@ def detection_targets_graph(proposals, gt_class_ids, gt_boxes, gt_masks, config)
 
 
     # Compute overlaps matrix [rpn_rois, gt_boxes]
-    # 1. Tile GT boxes and repeate ROIs tensor. This
-    # allows us to compare every ROI against every GT box without loops.
-    # TF doesn't have an equivalent to np.repeate() so simulate it
-    # using tf.tile() and tf.reshape.
-    rois = tf.reshape(tf.tile(tf.expand_dims(proposals, 1),
-                              [1, 1, tf.shape(gt_boxes)[0]]), [-1, 4])
-    boxes = tf.tile(gt_boxes, [tf.shape(proposals)[0], 1])
-    # 2. Compute intersections
-    roi_y1, roi_x1, roi_y2, roi_x2 = tf.split(rois, 4, axis=1)
-    box_y1, box_x1, box_y2, box_x2 = tf.split(boxes, 4, axis=1)
-    y1 = tf.maximum(roi_y1, box_y1)
-    x1 = tf.maximum(roi_x1, box_x1)
-    y2 = tf.minimum(roi_y2, box_y2)
-    x2 = tf.minimum(roi_x2, box_x2)
-    intersection = tf.maximum(x2 - x1, 0) * tf.maximum(y2 - y1, 0)
-    # 3. Compute unions
-    roi_area = (roi_y2 - roi_y1) * (roi_x2 - roi_x1)
-    box_area = (box_y2 - box_y1) * (box_x2 - box_x1)
-    union = roi_area + box_area - intersection
-    # 4. Compute IoU and reshape to [rois, boxes]
-    iou = intersection / union
-    overlaps = tf.reshape(iou, [tf.shape(proposals)[0], tf.shape(gt_boxes)[0]])
+    overlaps = overlaps_graph(proposals, gt_boxes)
 
     # Determine postive and negative ROIs
     roi_iou_max = tf.reduce_max(overlaps, axis=1)
