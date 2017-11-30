d0f6eebeec524a6afa5ce7f4a7c7b4d70a0f7ca9
Petrichor <hok2016jn@gmail.com>
2017-11-30 21:04:09 +0800

    Fix bug: cannot display instances

--- visualize.py
+++ visualize.py
@@ -143,7 +143,8 @@ def display_instances(image, boxes, masks, class_ids, class_names,
             p = Polygon(verts, facecolor="none", edgecolor=color)
             ax.add_patch(p)
     ax.imshow(masked_image.astype(np.uint8))
-
+    plt.show()
+    
 
 def draw_rois(image, rois, refined_rois, mask, class_ids, class_names, limit=10):
     """
