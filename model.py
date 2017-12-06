731fc74c0717079a8b2891c70fd3bde874623af8
Pavlos Melissinos <pmelissi@iti.gr>
2017-12-06 15:12:22 +0200

    Remove unnecessary function call from within a loop

--- model.py
+++ model.py
@@ -753,8 +753,8 @@ class DetectionLayer(KE.Layer):
     def call(self, inputs):
         def wrapper(rois, mrcnn_class, mrcnn_bbox, image_meta):
             detections_batch = []
+            _, _, window, _ = parse_image_meta(image_meta)
             for b in range(self.config.BATCH_SIZE):
-                _, _, window, _ = parse_image_meta(image_meta)
                 detections = refine_detections(
                     rois[b], mrcnn_class[b], mrcnn_bbox[b], window[b], self.config)
                 # Pad with zeros if detections < DETECTION_MAX_INSTANCES
