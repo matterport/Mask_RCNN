69acd2b01c36dc58177f0940bd07705be5b6ed41
Leo Han <hanl>
2018-03-13 21:03:56 -0400

    class_ids needs update together with masks

--- model.py
+++ model.py
@@ -1207,7 +1207,7 @@ def load_image_gt(dataset, config, image_id, augment=False,
     # and here is to filter them out
     _idx = np.sum(mask, axis=(0, 1)) > 0
     mask = mask[:, :, _idx]
-
+    class_ids = class_ids[_idx]
     # Bounding boxes. Note that some boxes might be all zeros
     # if the corresponding mask got cropped out.
     # bbox: [num_instances, (y1, x1, y2, x2)]
