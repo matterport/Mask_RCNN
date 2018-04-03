0a3d49fc1ecbf82eda3d27cfab2dc1dad1515487
Waleed Abdulla <waleed.abdulla@gmail.com>
2018-04-02 17:48:07 -0700

    Add dtype to printed array info in log().

--- model.py
+++ model.py
@@ -47,10 +47,11 @@ def log(text, array=None):
     """
     if array is not None:
         text = text.ljust(25)
-        text += ("shape: {:20}  min: {:10.5f}  max: {:10.5f}".format(
+        text += ("shape: {:20}  min: {:10.5f}  max: {:10.5f}  {}".format(
             str(array.shape),
             array.min() if array.size else "",
-            array.max() if array.size else ""))
+            array.max() if array.size else "",
+            array.dtype))
     print(text)
 
 
