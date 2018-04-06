0d0020923b12d7918b16aef81b6d066c4447d6b5
Gustav vZ <gustav.zitzewitz@gmx.de>
2018-04-06 15:13:12 +0200

    added mobilenet V1 weights

--- model.py
+++ model.py
@@ -2622,13 +2622,21 @@ class MaskRCNN():
         Returns path to weights file.
         """
         from keras.utils.data_utils import get_file
-        TF_WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/'\
-                                 'releases/download/v0.2/'\
-                                 'resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
-        weights_path = get_file('resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
-                                TF_WEIGHTS_PATH_NO_TOP,
-                                cache_subdir='models',
-                                md5_hash='a268eb855778b3df3c7506639542a6af')
+        if arch == "resnet50":
+            TF_WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/'\
+                                     'releases/download/v0.2/'\
+                                     'resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
+            weights_path = get_file('resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5',
+                                    TF_WEIGHTS_PATH_NO_TOP,
+                                    cache_subdir='models',
+                                    md5_hash='a268eb855778b3df3c7506639542a6af')
+        elif arch == "mobilenetv1":
+            TF_WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/'\
+                                     'releases/download/v0.6/mobilenet_1_0_224_tf.h5'
+            weights_path = get_file('mobilenet_1_0_224_tf.h5',
+                                    TF_WEIGHTS_PATH_NO_TOP,
+                                    cache_subdir='models',
+                                    md5_hash='03394917f9a9ea3362f46332a5b6a215')
         return weights_path
 
     def compile(self, learning_rate, momentum):
