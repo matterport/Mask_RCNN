ba0c8ba3a22b235101bcd4a9441f7b2f603a8b7d
Waleed Abdulla <waleed.abdulla@gmail.com>
2018-04-09 00:20:35 -0700

    Map from source image ID to internal ID
    The Dataset class assigns int image IDs to all
    images, but keeps track of their original ID from
    the source dataset. It’s easy to get the source ID
    if you have the image_id. This new mapping makes
    it also easy to get the image_id if you have the
    source id.

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -303,8 +303,11 @@ class Dataset(object):
         self.num_images = len(self.image_info)
         self._image_ids = np.arange(self.num_images)
 
+        # Mapping from source class and image IDs to internal IDs
         self.class_from_source_map = {"{}.{}".format(info['source'], info['id']): id
                                       for info, id in zip(self.class_info, self.class_ids)}
+        self.image_from_source_map = {"{}.{}".format(info['source'], info['id']): id
+                                      for info, id in zip(self.image_info, self.image_ids)}
 
         # Map sources to class_ids they support
         self.sources = list(set([i['source'] for i in self.class_info]))
