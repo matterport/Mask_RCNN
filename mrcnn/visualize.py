4e737a64881d170d87a514f0fc3c9ba0591bf465
huy.luu <huy.luu@personifyinc.com>
2018-05-04 14:36:21 +0700

    working with py2

--- mrcnn/visualize.py
+++ mrcnn/visualize.py
@@ -64,7 +64,7 @@ def random_colors(N, bright=True):
     convert to RGB.
     """
     brightness = 1.0 if bright else 0.7
-    hsv = [(i / N, 1, brightness) for i in range(N)]
+    hsv = [(float(i) / N, 1, brightness) for i in range(N)]
     colors = list(map(lambda c: colorsys.hsv_to_rgb(*c), hsv))
     random.shuffle(colors)
     return colors
