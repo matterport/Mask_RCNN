f9b1f244947f50470346fd3ec6da7691d6055d5b
ziyi <ziyigogogo@gmail.com>
2018-05-10 17:12:08 +0800

    fix "No such file or directory" if not use: "keras.callbacks.TensorBoard"

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2235,6 +2235,10 @@ class MaskRCNN():
         self.log_dir = os.path.join(self.model_dir, "{}{:%Y%m%dT%H%M}".format(
             self.config.NAME.lower(), now))
 
+        # Create log_dir if not exists
+        if not os.path.exists(self.log_dir):
+            os.makedirs(self.log_dir)
+
         # Path to save after each epoch. Include placeholders that get filled by Keras.
         self.checkpoint_path = os.path.join(self.log_dir, "mask_rcnn_{}_*epoch*.h5".format(
             self.config.NAME.lower()))
