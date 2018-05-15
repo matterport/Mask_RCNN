d6cffe4df0181fcf4b5ff38decaddfaad697c0c1
Julien Rebetez <julien.rebetez@picterra.ch>
2018-05-15 11:48:32 +0200

    Allow custom backbone implementation through Config.BACKBONE
    This allows one to set a callable in Config.BACKBONE to use a custom
    backbone model.

--- mrcnn/config.py
+++ mrcnn/config.py
@@ -49,8 +49,16 @@ class Config(object):
     VALIDATION_STEPS = 50
 
     # Backbone network architecture
-    # Supported values are: resnet50, resnet101
+    # Supported values are: resnet50, resnet101.
+    # You can also provide a callable that should have the signature
+    # of model.resnet_graph. If you do so, you need to supply a callable
+    # to COMPUTE_BACKBONE_SHAPE as well
     BACKBONE = "resnet101"
+
+    # Only useful if you supply a callable to BACKBONE. Should compute
+    # the shape of each layer of the FPN Pyramid.
+    # See model.compute_backbone_shapes
+    COMPUTE_BACKBONE_SHAPE = None
 
     # The strides of each layer of the FPN Pyramid. These values
     # are based on a Resnet101 backbone.
