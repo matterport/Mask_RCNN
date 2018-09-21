80cac4c0d8d5665de1b4874a7e0bd718a9e6e689
Max Frei <36265931+maxfrei750@users.noreply.github.com>
2018-09-21 10:25:29 +0200

    Fixed comment on GPU_COUNT (#878)
    Fixed comment on GPU_COUNT

--- mrcnn/config.py
+++ mrcnn/config.py
@@ -24,7 +24,7 @@ class Config(object):
     # experiment is running.
     NAME = None  # Override in sub-classes
 
-    # NUMBER OF GPUs to use. For CPU training, use 1
+    # NUMBER OF GPUs to use. When using only a CPU, this needs to be set to 1.
     GPU_COUNT = 1
 
     # Number of images to train with on each GPU. A 12GB GPU can typically
