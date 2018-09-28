1aca439c37849dcd085167c4e69d3abcd9d368d7
keineahnung2345 <mimifasosofamire1123@gmail.com>
2018-09-29 07:54:59 +0800

    Fix the input shape of anchors in ProposalLayer (#961)
    Fix the input shape of anchors in ProposalLayer and use "num_anchors" to replace "anchors" to avoid confusing

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -258,9 +258,9 @@ class ProposalLayer(KE.Layer):
     box refinement deltas to anchors.
 
     Inputs:
-        rpn_probs: [batch, anchors, (bg prob, fg prob)]
-        rpn_bbox: [batch, anchors, (dy, dx, log(dh), log(dw))]
-        anchors: [batch, (y1, x1, y2, x2)] anchors in normalized coordinates
+        rpn_probs: [batch, num_anchors, (bg prob, fg prob)]
+        rpn_bbox: [batch, num_anchors, (dy, dx, log(dh), log(dw))]
+        anchors: [batch, num_anchors, (y1, x1, y2, x2)] anchors in normalized coordinates
 
     Returns:
         Proposals in normalized coordinates [batch, rois, (y1, x1, y2, x2)]
