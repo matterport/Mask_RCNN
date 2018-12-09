f652ea05e3d3532503658d8f20960a3991b271c8
Asher Yartsev <>
2018-12-09 22:43:26 +0200

    updated to perform 1 step and 1 epoch on 2 gpu - for sanity checks

--- cucu_train/cucuTrain.py
+++ cucu_train/cucuTrain.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:973acc1ea0038a609d43278e1d22ab10742b2bca7a65940259bfb6c22ddd2cf4
-size 27359
+oid sha256:58b2cde8e4b34490ad34249d4a3535cab72a1b80cbb5df59879a84831868e591
+size 27358
