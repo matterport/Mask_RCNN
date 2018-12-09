4efb368e6e715d7ee8fa82f4ce090fe92d1c8034
Asher Yartsev <>
2018-12-09 22:32:40 +0200

    small changes to prepare for server-running

--- cucu_train/cucuTrain.py
+++ cucu_train/cucuTrain.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:90c8552e99ca1cc148c9ed6ae114c2dcbc27d17d1bb5a82be6ed06b850131667
-size 27201
+oid sha256:973acc1ea0038a609d43278e1d22ab10742b2bca7a65940259bfb6c22ddd2cf4
+size 27359
