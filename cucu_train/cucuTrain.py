d3e2a5413cc8bab018219b75baf4f002510b98b8
Asher Yartsev <>
2018-12-09 23:09:18 +0200

    commiting again the corrupted files

--- cucu_train/cucuTrain.py
+++ cucu_train/cucuTrain.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:58b2cde8e4b34490ad34249d4a3535cab72a1b80cbb5df59879a84831868e591
-size 27358
+oid sha256:0554fd7aadbd162d3f6b25af813243dd103a4509394d949a4637e5313ac433ce
+size 27364
