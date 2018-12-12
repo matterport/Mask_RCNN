338391642d4c65f02a27259a447ef9337281ba44
Asher Yartsev <>
2018-12-12 12:19:14 +0200

    minor change

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -19,7 +19,7 @@ import numpy as np
 import tensorflow as tf
 
 config = tf.ConfigProto()
-# config.gpu_options.allow_growth = True
+config.gpu_options.allow_growth = True
 config.gpu_options.per_process_gpu_memory_fraction = 0.1
 config.gpu_options.visible_device_list = "0"
 # set_session(tf.Session(config=config))
