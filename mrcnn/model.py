0e9ddd85dc852b63997416bc2a0aa585d2de7ded
Asher Yartsev <>
2018-12-12 13:47:16 +0200

    minor change

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -20,7 +20,7 @@ import tensorflow as tf
 
 config = tf.ConfigProto()
 # config.gpu_options.allow_growth = True
-config.gpu_options.per_process_gpu_memory_fraction = 0.4
+config.gpu_options.per_process_gpu_memory_fraction = 0.2
 config.gpu_options.visible_device_list = "0"
 # set_session(tf.Session(config=config))
 tf.Session(config=config)
@@ -2387,8 +2387,8 @@ class MaskRCNN():
             validation_data=val_generator,
             validation_steps=self.config.VALIDATION_STEPS,
             max_queue_size=100,
-            workers=workers,
-            use_multiprocessing=True,
+            workers=1,
+            use_multiprocessing=False,
         )
         self.epoch = max(self.epoch, epochs)
 
