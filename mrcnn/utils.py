551905bb4882d32c1dbe9f0d2ac09b587fac20a8
Asher Yartsev <>
2018-12-12 14:03:48 +0200

    minor change

--- mrcnn/utils.py
+++ mrcnn/utils.py
@@ -13,6 +13,14 @@ import math
 import random
 import numpy as np
 import tensorflow as tf
+
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+config.gpu_options.per_process_gpu_memory_fraction = 0.1
+config.gpu_options.visible_device_list = "0"
+# set_session(tf.Session(config=config))
+tf.Session(config=config)
+
 import scipy
 import skimage.color
 import skimage.io
