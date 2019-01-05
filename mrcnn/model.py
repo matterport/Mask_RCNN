64f15f9bb79c6889e74c193be02f690ad1b5f7d8
Asher Yartsev <>
2019-01-05 14:13:09 +0200

    added scope for new summary scalars

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2387,68 +2387,69 @@ class MaskRCNN():
             def set_model(self, model):
                 self.model = model
                 self.sess = K.get_session()
-                if self.histogram_freq and self.merged is None:
-                    for layer in self.model.layers:
+                with tf.name_scope('performance'):
+                    if self.histogram_freq and self.merged is None:
+                        for layer in self.model.layers:
 
-                        for weight in layer.weights:
-                            mapped_weight_name = weight.name.replace(':', '_')
-                            tf.summary.histogram(mapped_weight_name, weight)
-                            if self.write_grads:
-                                grads = model.optimizer.get_gradients(model.total_loss,
-                                                                    weight)
+                            for weight in layer.weights:
+                                mapped_weight_name = weight.name.replace(':', '_')
+                                tf.summary.histogram(mapped_weight_name, weight)
+                                if self.write_grads:
+                                    grads = model.optimizer.get_gradients(model.total_loss,
+                                                                        weight)
 
-                                def is_indexed_slices(grad):
-                                    return type(grad).__name__ == 'IndexedSlices'
-                                grads = [
-                                    grad.values if is_indexed_slices(grad) else grad
-                                    for grad in grads]
-                                tf.summary.histogram('{}_grad'.format(mapped_weight_name), grads)
-                            if self.write_images:
-                                w_img = tf.squeeze(weight)
-                                shape = K.int_shape(w_img)
-                                if len(shape) == 2:  # dense layer kernel case
-                                    if shape[0] > shape[1]:
-                                        w_img = tf.transpose(w_img)
-                                        shape = K.int_shape(w_img)
-                                    w_img = tf.reshape(w_img, [1,
-                                                            shape[0],
-                                                            shape[1],
-                                                            1])
-                                elif len(shape) == 3:  # convnet case
-                                    if K.image_data_format() == 'channels_last':
-                                        # switch to channels_first to display
-                                        # every kernel as a separate image
-                                        w_img = tf.transpose(w_img, perm=[2, 0, 1])
-                                        shape = K.int_shape(w_img)
-                                    w_img = tf.reshape(w_img, [shape[0],
-                                                            shape[1],
-                                                            shape[2],
-                                                            1])
-                                elif len(shape) == 1:  # bias case
-                                    w_img = tf.reshape(w_img, [1,
-                                                            shape[0],
-                                                            1,
-                                                            1])
-                                else:
-                                    # not possible to handle 3D convnets etc.
-                                    continue
+                                    def is_indexed_slices(grad):
+                                        return type(grad).__name__ == 'IndexedSlices'
+                                    grads = [
+                                        grad.values if is_indexed_slices(grad) else grad
+                                        for grad in grads]
+                                    tf.summary.histogram('{}_grad'.format(mapped_weight_name), grads)
+                                if self.write_images:
+                                    w_img = tf.squeeze(weight)
+                                    shape = K.int_shape(w_img)
+                                    if len(shape) == 2:  # dense layer kernel case
+                                        if shape[0] > shape[1]:
+                                            w_img = tf.transpose(w_img)
+                                            shape = K.int_shape(w_img)
+                                        w_img = tf.reshape(w_img, [1,
+                                                                shape[0],
+                                                                shape[1],
+                                                                1])
+                                    elif len(shape) == 3:  # convnet case
+                                        if K.image_data_format() == 'channels_last':
+                                            # switch to channels_first to display
+                                            # every kernel as a separate image
+                                            w_img = tf.transpose(w_img, perm=[2, 0, 1])
+                                            shape = K.int_shape(w_img)
+                                        w_img = tf.reshape(w_img, [shape[0],
+                                                                shape[1],
+                                                                shape[2],
+                                                                1])
+                                    elif len(shape) == 1:  # bias case
+                                        w_img = tf.reshape(w_img, [1,
+                                                                shape[0],
+                                                                1,
+                                                                1])
+                                    else:
+                                        # not possible to handle 3D convnets etc.
+                                        continue
 
-                                shape = K.int_shape(w_img)
-                                assert len(shape) == 4 and shape[-1] in [1, 3, 4]
-                                tf.summary.image(mapped_weight_name, w_img)
+                                    shape = K.int_shape(w_img)
+                                    assert len(shape) == 4 and shape[-1] in [1, 3, 4]
+                                    tf.summary.image(mapped_weight_name, w_img)
 
-                        # if hasattr(layer, 'output'):
-                        #     if(layer.output.dtype == bool):
-                        #         continue
-                        #     tf.summary.histogram('{}_out'.format(layer.name),
-                        #                         layer.output)
-                self.merged = tf.summary.merge_all()
+                            # if hasattr(layer, 'output'):
+                            #     if(layer.output.dtype == bool):
+                            #         continue
+                            #     tf.summary.histogram('{}_out'.format(layer.name),
+                            #                         layer.output)
+                    self.merged = tf.summary.merge_all()
 
-                if self.write_graph:
-                    self.writer = tf.summary.FileWriter(self.log_dir,
-                                                        self.sess.graph)
-                else:
-                    self.writer = tf.summary.FileWriter(self.log_dir)
+                    if self.write_graph:
+                        self.writer = tf.summary.FileWriter(self.log_dir,
+                                                            self.sess.graph)
+                    else:
+                        self.writer = tf.summary.FileWriter(self.log_dir)
 
             def on_epoch_end(self, epoch, logs=None):
                 logs = logs or {}
