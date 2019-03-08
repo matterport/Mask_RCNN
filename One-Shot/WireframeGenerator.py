7f5bb4586dc630720570162120d5f9a284719b63
Holstrup <abho@dtu.dk>
2019-03-08 16:05:01 +0100

    * Implemented image generator that generate rectangluar masks * wireframe.py should also work now once the generator is changed to generate polygons instead of rectangles

--- One-Shot/WireframeGenerator.py
+++ One-Shot/WireframeGenerator.py
@@ -2,8 +2,9 @@ import os
 from PIL import Image
 import random
 
-ICON_DIR = os.getcwd() + "/Icons"
-ICONS = os.listdir(ICON_DIR)
+ICONS = os.listdir(os.getcwd() + "/Icons")
+MASK_DIR = os.path.abspath(os.path.join(os.getcwd(),"../"))
+DATA_DIR = os.path.abspath(os.path.join(MASK_DIR, "datasets/wireframe"))
 
 # Wire-frame dimensions (iPhone 8)
 DIMENSIONS = (750, 1334)
@@ -13,37 +14,51 @@ COLOR = (255, 255, 255, 255)
 # Icon Dimensions
 ICON_W = ICON_H = 50
 
+#Specify training data or validation data
+#DATA_TYPE = "/train"
+DATA_TYPE = "/val"
 
-def add_box_json(filename, size, x, y, width, height, label):
-    s = " '{}{}':{{ 'filename': '{}', 'size': {}, 'regions': " \
-        "[{{ 'shape_attributes': {{'name':'rect','x':{},'y':{},'width':{},'height':{}}}, " \
-        "'region_attributes':{{'name':{} }} }}] }}"
+file_content = "{"
 
-    s = s.format(filename, size, filename, size, x, y, width, height, label)
+
+def add_box_json(filename, size, icons):
+    s = " '{}{}':{{ 'filename': '{}', 'size': {}, 'regions': [".format(filename, size, filename, size)
+    for icon in icons:
+        s += "{{'shape_attributes': {{'name': 'rect', 'x': {}, 'y': {}, 'width': 50, 'height': 50}}, " \
+                "'region_attributes': {{'name': '{}'}} }} ,".format(icon[1], icon[2], icon[0])
+    s = s[0:-1] + "]" + " },"
     s = s.replace("'", '"')
     return s
 
 
-for j in range(10):
-    NUM_ICONS = random.randint(1, 3)
-    background = Image.new('RGBA', DIMENSIONS, COLOR)
-    for i in range(NUM_ICONS):
-        cur_icon = ICONS[random.randint(0, len(ICONS) - 1)]
-        img = Image.open('Icons/' + cur_icon, 'r').resize((50, 50))
-        offset = random.randint(1, bg_w - ICON_W), random.randint(1, bg_h - ICON_H)
-        background.paste(img, offset)
-    img_dir_name = 'TrainingData/' + str(j) + ".png"
-    background.save(img_dir_name)
-
-    filename = str(j) + ".png"
-    img_size = os.stat(img_dir_name).st_size
-    x, y = offset
-    x -= 10
-    y -= 10
-    height = ICON_H + 20
-    width = ICON_W + 20
-    cur_icon
-    print(add_box_json(filename, img_size, x, y, width, height, cur_icon))
-    break
+def write_string_to_json(string):
+    with open(DATA_DIR + DATA_TYPE + '/via_region_data.json', 'w+') as f:
+        f.write(string)
 
 
+def generate_data(NUM_IMAGES=10, ICONS_PER_IMAGE=3):
+    global file_content
+
+    for j in range(NUM_IMAGES):
+        icon_list = []
+        NUM_ICONS = random.randint(1, ICONS_PER_IMAGE)
+
+        background = Image.new('RGBA', DIMENSIONS, COLOR)
+        for i in range(NUM_ICONS):
+            cur_icon = ICONS[random.randint(0, len(ICONS) - 1)]
+            img = Image.open('Icons/' + cur_icon, 'r').resize((ICON_W, ICON_H))
+            offset = random.randint(1, bg_w - ICON_W), random.randint(1, bg_h - ICON_H)
+            background.paste(img, offset)
+            icon_list.append((cur_icon[0:-4], offset[0], offset[1]))
+
+        img_dir_name = DATA_DIR + DATA_TYPE + '/' + str(j) + ".png"
+        background.save(img_dir_name)
+        filename = str(j) + ".png"
+        img_size = os.stat(img_dir_name).st_size
+        file_content += add_box_json(filename, img_size, icon_list)
+
+    file_content = file_content[0:-1] + "}"
+    write_string_to_json(file_content)
+
+
+generate_data()
--- One-Shot/wireframe.py
+++ One-Shot/wireframe.py
@@ -35,7 +35,7 @@ import numpy as np
 import skimage.draw
 
 # Root directory of the project
-ROOT_DIR = os.path.abspath("../../")
+ROOT_DIR = os.path.abspath("../")
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
@@ -54,19 +54,19 @@ DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
 ############################################################
 
 
-class BalloonConfig(Config):
+class WireframeConfig(Config):
     """Configuration for training on the toy  dataset.
     Derives from the base Config class and overrides some values.
     """
     # Give the configuration a recognizable name
-    NAME = "balloon"
+    NAME = "Wireframe"
 
     # We use a GPU with 12GB memory, which can fit two images.
     # Adjust down if you use a smaller GPU.
     IMAGES_PER_GPU = 2
 
     # Number of classes (including background)
-    NUM_CLASSES = 1 + 1  # Background + balloon
+    NUM_CLASSES = 1 + 4  # Background + Icons
 
     # Number of training steps per epoch
     STEPS_PER_EPOCH = 100
@@ -79,15 +79,18 @@ class BalloonConfig(Config):
 #  Dataset
 ############################################################
 
-class BalloonDataset(utils.Dataset):
+class WireframeDataset(utils.Dataset):
 
-    def load_balloon(self, dataset_dir, subset):
+    def load_wireframe(self, dataset_dir, subset):
         """Load a subset of the Balloon dataset.
         dataset_dir: Root directory of the dataset.
         subset: Subset to load: train or val
         """
         # Add classes. We have only one class to add.
-        self.add_class("balloon", 1, "balloon")
+        self.add_class("Cloud", 1, "Cloud")
+        self.add_class("Cross", 2, "Cross")
+        self.add_class("Menu", 3, "Menu")
+        self.add_class("More", 4, "More")
 
         # Train or validation dataset?
         assert subset in ["train", "val"]
@@ -125,7 +128,7 @@ class BalloonDataset(utils.Dataset):
             if type(a['regions']) is dict:
                 polygons = [r['shape_attributes'] for r in a['regions'].values()]
             else:
-                polygons = [r['shape_attributes'] for r in a['regions']] 
+                polygons = [r['shape_attributes'] for r in a['regions']]
 
             # load_mask() needs the image size to convert polygons to masks.
             # Unfortunately, VIA doesn't include it in JSON, so we must read
@@ -135,7 +138,7 @@ class BalloonDataset(utils.Dataset):
             height, width = image.shape[:2]
 
             self.add_image(
-                "balloon",
+                "Wireframe",
                 image_id=a['filename'],  # use file name as a unique image id
                 path=image_path,
                 width=width, height=height,
@@ -150,7 +153,7 @@ class BalloonDataset(utils.Dataset):
         """
         # If not a balloon dataset image, delegate to parent class.
         image_info = self.image_info[image_id]
-        if image_info["source"] != "balloon":
+        if image_info["source"] != "Wireframe":
             return super(self.__class__, self).load_mask(image_id)
 
         # Convert polygons to a bitmap mask of shape
@@ -160,7 +163,8 @@ class BalloonDataset(utils.Dataset):
                         dtype=np.uint8)
         for i, p in enumerate(info["polygons"]):
             # Get indexes of pixels inside the polygon and set them to 1
-            rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
+            rr, cc = skimage.draw.rectangle(p['all_points_y'], p['all_points_x'])
+            #rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
             mask[rr, cc, i] = 1
 
         # Return mask, and array of class IDs of each instance. Since we have
@@ -170,7 +174,7 @@ class BalloonDataset(utils.Dataset):
     def image_reference(self, image_id):
         """Return the path of the image."""
         info = self.image_info[image_id]
-        if info["source"] == "balloon":
+        if info["source"] == "Wireframe":
             return info["path"]
         else:
             super(self.__class__, self).image_reference(image_id)
@@ -179,13 +183,13 @@ class BalloonDataset(utils.Dataset):
 def train(model):
     """Train the model."""
     # Training dataset.
-    dataset_train = BalloonDataset()
-    dataset_train.load_balloon(args.dataset, "train")
+    dataset_train = WireframeDataset()
+    dataset_train.load_wireframe(args.dataset, "train")
     dataset_train.prepare()
 
     # Validation dataset
-    dataset_val = BalloonDataset()
-    dataset_val.load_balloon(args.dataset, "val")
+    dataset_val = WireframeDataset()
+    dataset_val.load_wireframe(args.dataset, "val")
     dataset_val.prepare()
 
     # *** This training schedule is an example. Update to your needs ***
@@ -199,78 +203,6 @@ def train(model):
                 layers='heads')
 
 
-def color_splash(image, mask):
-    """Apply color splash effect.
-    image: RGB image [height, width, 3]
-    mask: instance segmentation mask [height, width, instance count]
-
-    Returns result image.
-    """
-    # Make a grayscale copy of the image. The grayscale copy still
-    # has 3 RGB channels, though.
-    gray = skimage.color.gray2rgb(skimage.color.rgb2gray(image)) * 255
-    # Copy color pixels from the original color image where mask is set
-    if mask.shape[-1] > 0:
-        # We're treating all instances as one, so collapse the mask into one layer
-        mask = (np.sum(mask, -1, keepdims=True) >= 1)
-        splash = np.where(mask, image, gray).astype(np.uint8)
-    else:
-        splash = gray.astype(np.uint8)
-    return splash
-
-
-def detect_and_color_splash(model, image_path=None, video_path=None):
-    assert image_path or video_path
-
-    # Image or video?
-    if image_path:
-        # Run model detection and generate the color splash effect
-        print("Running on {}".format(args.image))
-        # Read image
-        image = skimage.io.imread(args.image)
-        # Detect objects
-        r = model.detect([image], verbose=1)[0]
-        # Color splash
-        splash = color_splash(image, r['masks'])
-        # Save output
-        file_name = "splash_{:%Y%m%dT%H%M%S}.png".format(datetime.datetime.now())
-        skimage.io.imsave(file_name, splash)
-    elif video_path:
-        import cv2
-        # Video capture
-        vcapture = cv2.VideoCapture(video_path)
-        width = int(vcapture.get(cv2.CAP_PROP_FRAME_WIDTH))
-        height = int(vcapture.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        fps = vcapture.get(cv2.CAP_PROP_FPS)
-
-        # Define codec and create video writer
-        file_name = "splash_{:%Y%m%dT%H%M%S}.avi".format(datetime.datetime.now())
-        vwriter = cv2.VideoWriter(file_name,
-                                  cv2.VideoWriter_fourcc(*'MJPG'),
-                                  fps, (width, height))
-
-        count = 0
-        success = True
-        while success:
-            print("frame: ", count)
-            # Read next image
-            success, image = vcapture.read()
-            if success:
-                # OpenCV returns images as BGR, convert to RGB
-                image = image[..., ::-1]
-                # Detect objects
-                r = model.detect([image], verbose=0)[0]
-                # Color splash
-                splash = color_splash(image, r['masks'])
-                # RGB -> BGR to save image to video
-                splash = splash[..., ::-1]
-                # Add image to video writer
-                vwriter.write(splash)
-                count += 1
-        vwriter.release()
-    print("Saved to ", file_name)
-
-
 ############################################################
 #  Training
 ############################################################
@@ -281,9 +213,6 @@ if __name__ == '__main__':
     # Parse command line arguments
     parser = argparse.ArgumentParser(
         description='Train Mask R-CNN to detect balloons.')
-    parser.add_argument("command",
-                        metavar="<command>",
-                        help="'train' or 'splash'")
     parser.add_argument('--dataset', required=False,
                         metavar="/path/to/balloon/dataset/",
                         help='Directory of the Balloon dataset')
@@ -294,44 +223,20 @@ if __name__ == '__main__':
                         default=DEFAULT_LOGS_DIR,
                         metavar="/path/to/logs/",
                         help='Logs and checkpoints directory (default=logs/)')
-    parser.add_argument('--image', required=False,
-                        metavar="path or URL to image",
-                        help='Image to apply the color splash effect on')
-    parser.add_argument('--video', required=False,
-                        metavar="path or URL to video",
-                        help='Video to apply the color splash effect on')
     args = parser.parse_args()
 
     # Validate arguments
-    if args.command == "train":
-        assert args.dataset, "Argument --dataset is required for training"
-    elif args.command == "splash":
-        assert args.image or args.video,\
-               "Provide --image or --video to apply color splash"
+    assert args.dataset, "Argument --dataset is required for training"
 
     print("Weights: ", args.weights)
     print("Dataset: ", args.dataset)
     print("Logs: ", args.logs)
-
     # Configurations
-    if args.command == "train":
-        config = BalloonConfig()
-    else:
-        class InferenceConfig(BalloonConfig):
-            # Set batch size to 1 since we'll be running inference on
-            # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
-            GPU_COUNT = 1
-            IMAGES_PER_GPU = 1
-        config = InferenceConfig()
+    config = WireframeConfig()
     config.display()
 
     # Create model
-    if args.command == "train":
-        model = modellib.MaskRCNN(mode="training", config=config,
-                                  model_dir=args.logs)
-    else:
-        model = modellib.MaskRCNN(mode="inference", config=config,
-                                  model_dir=args.logs)
+    model = modellib.MaskRCNN(mode="training", config=config, model_dir=args.logs)
 
     # Select weights file to load
     if args.weights.lower() == "coco":
@@ -360,12 +265,5 @@ if __name__ == '__main__':
         model.load_weights(weights_path, by_name=True)
 
     # Train or evaluate
-    if args.command == "train":
-        train(model)
-    elif args.command == "splash":
-        detect_and_color_splash(model, image_path=args.image,
-                                video_path=args.video)
-    else:
-        print("'{}' is not recognized. "
-              "Use 'train' or 'splash'".format(args.command))
+    train(model)
 
