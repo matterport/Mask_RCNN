4fa9c065e71893692c87d7eb58f428d13d28839a
Cristian Botezatu <s164571@student.dtu.dk>
2019-03-10 12:56:30 +0100

    Mask R-CNN for wireframes (jupyter notebook)

---
+++ samples/wireframe/wireframe_new.py
@@ -0,0 +1,237 @@
+import os
+import sys
+import json
+import datetime
+import numpy as np
+import skimage.draw
+from matplotlib import pyplot as plt
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn.config import Config
+from mrcnn import model as modellib, utils
+from mrcnn import visualize
+# Path to trained weights file
+COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
+
+# Directory to save logs and model checkpoints, if not provided
+# through the command line argument --logs
+DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+############################################################
+#  Configurations
+############################################################
+
+
+class WireframeConfig(Config):
+    """Configuration for training on the toy dataset.
+    Derives from the base Config class and overrides some values.
+    """
+    # Give the configuration a recognizable name
+    NAME = "wireframe"
+
+    # We use a GPU with 12GB memory, which can fit two images.
+    # Adjust down if you use a smaller GPU.
+    IMAGES_PER_GPU = 2
+
+    # Number of classes (including background)
+    NUM_CLASSES = 1 + 4  # Background + objects
+
+    # Number of training steps per epoch
+    STEPS_PER_EPOCH = 100
+
+    # Skip detections with < 90% confidence
+    DETECTION_MIN_CONFIDENCE = 0.9
+
+
+############################################################
+#  Dataset
+############################################################
+
+class WireframeDataset(utils.Dataset):
+    def load_wireframe(self, dataset_dir, subset, hc=False):
+        """Load the surgery dataset from VIA.
+        dataset_dir: Root directory of the dataset.
+        subset: Subset to load: train or val or predict
+        """
+        # Add classes. We have only one class to add.
+        self.add_class("wireframe", 1, "Cloud")
+        self.add_class("wireframe", 2, "Cross")
+        self.add_class("wireframe", 3, "Menu")
+        self.add_class("wireframe", 4, "More")
+
+        # Train or validation dataset?
+        assert subset in ["train", "val", "predict"]
+        dataset_dir = os.path.join(dataset_dir, subset)
+
+        # We mostly care about the x and y coordinates of each region
+        annotations = json.load(open(os.path.join(dataset_dir, "via_region_data.json")))
+
+        annotations = list(annotations.values())  # don't need the dict keys
+        # The VIA tool saves images in the JSON even if they don't have any
+        # annotations. Skip unannotated images.
+        annotations = [a for a in annotations if a['regions']]
+
+        # Add images
+        for a in annotations:
+            # Get the x, y coordinaets of points of the polygons that make up
+            # the outline of each object instance. There are stores in the
+            # shape_attributes (see json format above)
+            if type(a['regions']) is dict:
+                polygons = [r['shape_attributes'] for r in a['regions'].values()]
+                names = [r['region_attributes'] for r in a['regions'].values()]
+            else:
+                polygons = [r['shape_attributes'] for r in a['regions']]
+                names = [r['region_attributes'] for r in a['regions']]
+            # load_mask() needs the image size to convert polygons to masks.
+            # Unfortunately, VIA doesn't include it in JSON, so we must read
+            # the image. This is only managable since the dataset is tiny.
+            image_path = os.path.join(dataset_dir, a['filename'])
+            image = skimage.io.imread(image_path)
+            height, width = image.shape[:2]
+
+            self.add_image(
+                "wireframe",
+                image_id=a['filename'],  # use file name as a unique image id
+                path=image_path,
+                width=width, height=height,
+                polygons=polygons,
+                names=names)
+
+    def load_mask(self, image_id):
+        """Generate instance masks for an image.
+       Returns:
+        masks: A bool array of shape [height, width, instance count] with
+            one mask per instance.
+        class_ids: a 1D array of class IDs of the instance masks.
+        """
+        # If not a surgery dataset image, delegate to parent class.
+        image_info = self.image_info[image_id]
+        if image_info["source"] != "wireframe":
+            return super(self.__class__, self).load_mask(image_id)
+
+        # Convert polygons to a bitmap mask of shape
+        # [height, width, instance_count]
+        info = self.image_info[image_id]
+        class_names = info["names"]
+        mask = np.zeros([info["height"], info["width"], len(info["polygons"])],
+                        dtype=np.uint8)
+        for i, p in enumerate(info["polygons"]):
+            # Get indexes of pixels inside the polygon and set them to 1
+            rr, cc = skimage.draw.polygon(p['all_points_y'], p['all_points_x'])
+            mask[rr, cc, i] = 1
+        # Assign class_ids by reading class_names
+        class_ids = np.zeros([len(info["polygons"])])
+        # In the surgery dataset, pictures are labeled with name 'a' and 'r' representing arm and ring.
+        for i, p in enumerate(class_names):
+        #"name" is the attributes name decided when labeling, etc. 'region_attributes': {name:'a'}
+            if p['name'] == 'Cloud':
+                class_ids[i] = 1
+            elif p['name'] == 'Cross':
+                class_ids[i] = 2
+            elif p['name'] == 'Menu':
+                class_ids[i] = 3
+            elif p['name'] == 'More':
+                class_ids[i] = 4
+            #assert code here to extend to other labels
+        class_ids = class_ids.astype(int)
+        # Return mask, and array of class IDs of each instance. Since we have
+        # one class ID only, we return an array of 1s
+        return mask.astype(np.bool), class_ids
+
+    def image_reference(self, image_id):
+        """Return the path of the image."""
+        info = self.image_info[image_id]
+        if info["source"] == "wireframe":
+            return info["path"]
+        else:
+            super(self.__class__, self).image_reference(image_id)
+
+def train(model):
+    """Train the model."""
+    # Training dataset.
+    dataset_train = WireframeDataset()
+    dataset_train.load_wireframe(args.dataset, "train")
+    dataset_train.prepare()
+
+    # Validation dataset
+    dataset_val = WireframeDataset()
+    dataset_val.load_wireframe(args.dataset, "val")
+    dataset_val.prepare()
+
+    # *** This training schedule is an example. Update to your needs ***
+    # Since we're using a very small dataset, and starting from
+    # COCO trained weights, we don't need to train too long. Also,
+    # no need to train all layers, just the heads should do it.
+    print("Training network heads")
+    model.train(dataset_train, dataset_val,
+                learning_rate=config.LEARNING_RATE,
+                epochs=30,
+                layers='heads')
+
+
+############################################################
+#  Training
+############################################################
+
+if __name__ == '__main__':
+    import argparse
+
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(
+        description='Train Mask R-CNN to detect balloons.')
+    parser.add_argument('--dataset', required=False,
+                        metavar="/Users/BotezatuCristian/PycharmProjects/Mask_RCNN/datasets/wireframe",
+                        help='Directory of the wireframe dataset')
+    parser.add_argument('--weights', required=True,
+                        metavar="/Users/BotezatuCristian/PycharmProjects/Mask_RCNN/mask_rcnn_coco.h5",
+                        help="Path to weights .h5 file or 'coco'")
+    parser.add_argument('--logs', required=False,
+                        default=DEFAULT_LOGS_DIR,
+                        metavar="/path/to/logs/",
+                        help='Logs and checkpoints directory (default=logs/)')
+    args = parser.parse_args()
+
+    # Validate arguments
+    assert args.dataset, "Argument --dataset is required for training"
+
+    print("Weights: ", args.weights)
+    print("Dataset: ", args.dataset)
+    print("Logs: ", args.logs)
+    # Configurations
+    config = WireframeConfig()
+    config.display()
+
+    # Create model
+    model = modellib.MaskRCNN(mode="training", config=config, model_dir=args.logs)
+
+    # Select weights file to load
+    if args.weights.lower() == "coco":
+        weights_path = COCO_WEIGHTS_PATH
+        # Download weights file
+        if not os.path.exists(weights_path):
+            utils.download_trained_weights(weights_path)
+    elif args.weights.lower() == "last":
+        # Find last trained weights
+        weights_path = model.find_last()
+    elif args.weights.lower() == "imagenet":
+        # Start from ImageNet trained weights
+        weights_path = model.get_imagenet_weights()
+    else:
+        weights_path = args.weights
+
+    # Load weights
+    print("Loading weights ", weights_path)
+    if args.weights.lower() == "coco":
+        # Exclude the last layers because they require a matching
+        # number of classes
+        model.load_weights(weights_path, by_name=True, exclude=[
+            "mrcnn_class_logits", "mrcnn_bbox_fc",
+            "mrcnn_bbox", "mrcnn_mask"])
+    else:
+        model.load_weights(weights_path, by_name=True)
+
+    # Train or evaluate
+    train(model)
