021cae11a94f5fcaa60e8d4f5b6b1b626dfabbe6
Holstrup <abho@dtu.dk>
2019-04-05 12:52:15 +0200

    Included loss functions

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2183,7 +2183,7 @@ class MaskRCNN():
         self.keras_model._per_input_losses = {}
         loss_names = [
             "rpn_class_loss",  "rpn_bbox_loss",
-            "mrcnn_class_loss", "mrcnn_bbox_loss", "mrcnn_mask_loss"]
+            "mrcnn_class_loss", "mrcnn_bbox_loss", "mrcnn_mask_loss", "embedding_loss"]
         for name in loss_names:
             layer = self.keras_model.get_layer(name)
             if layer.output in self.keras_model.losses:
