bbf466a90d87efc08cde20c3bbef74a843784d96
Cristian Botezatu <s164571@student.dtu.dk>
2019-04-14 14:59:39 +0200

    Chisq-LMNN for our project and refactoring

--- samples/wireframe/plots.py
+++ samples/wireframe/plots.py
@@ -50,14 +50,20 @@ def plot_confusion_matrix(y_true, y_pred,
     This function prints and plots the confusion matrix.
     Normalization can be applied by setting `normalize=True`.
     """
-    if not title:
-        if normalize:
-            title = 'Normalized confusion matrix'
-        else:
-            title = 'Confusion matrix, without normalization'
-
     # Compute confusion matrix
     cm = confusion_matrix(y_true, y_pred)
+    accuracy = 100 * cm.diagonal().sum() / cm.sum()
+    error_rate = 100 - accuracy
+
+    # Set the title
+    if not title:
+        if normalize:
+            title = 'Normalized confusion matrix \n (Accuracy: {0:.2f}%, Error Rate: {1:.2f}%)'.format(accuracy,
+                                                                                                       error_rate)
+        else:
+            title = 'Confusion matrix, without normalization \n (Accuracy: {0:.2f}%, Error Rate: {1:.2f}%)'.format(
+                accuracy, error_rate)
+
     # Only use the labels that appear in the data
     classes = unique_labels(y_true, y_pred)
     if normalize:
