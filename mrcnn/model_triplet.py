9ebf11d6c31bff9dbbf31e51c736203b7e358d0d
Cristian Botezatu <s164571@student.dtu.dk>
2019-04-23 20:53:22 +0200

    Separate model for triplet loss embedding optimization

--- mrcnn/model_triplet.py
+++ mrcnn/model_triplet.py
@@ -4,7 +4,6 @@ import keras.backend as K
 import keras.layers as KL
 from mrcnn.triplet_loss import batch_all_triplet_loss
 import tensorflow as tf
-from keras.losses import sparse_categorical_crossentropy
 
 class Model:
     def __init__(self):
@@ -19,7 +18,7 @@ class Model:
         return full_model
 
     def compile_model(self):
-        self.model.compile(optimizer='adam', loss=sparse_categorical_crossentropy,
+        self.model.compile(optimizer='adam', loss=triplet_loss,
                            metrics=['accuracy'])
 
 def triplet_loss(y_true, y_pred):
@@ -27,8 +26,6 @@ def triplet_loss(y_true, y_pred):
     print("Y Pred (embeddings) shape: {}".format(K.int_shape(y_pred)))
 
     # Reshape data
-    y_pred = K.squeeze(y_pred, axis=1)
-    y_pred = K.squeeze(y_pred, axis=1)
     y_true = tf.reshape(y_true, [-1])
     def_margin = tf.constant(1.0, dtype=tf.float32)
 
