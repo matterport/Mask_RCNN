dea80897160b4914134641f6a171e3ba5c2dd5d6
Ryan Avery <ravery@ucsb.edu>
2019-05-14 15:51:59 -0700

    updated env setup to not replace tensorflow-gpu with tensorflow

--- bash-scripts/setup_env.sh
+++ bash-scripts/setup_env.sh
@@ -3,6 +3,3 @@ conda update -n base -c defaults conda -y
 /data/anaconda/envs/py36/bin/conda env update -f ~/work/$1/cropmask-env.yml -y
 cd ~/work/$1/
 /data/anaconda/envs/py36/bin/python setup.py develop
-/data/anaconda/envs/py36/bin/pip uninstall tensorflow
-/data/anaconda/envs/py36/bin/conda install tensorflow-gpu
-/data/anaconda/envs/py36/bin/conda install tensorflow
