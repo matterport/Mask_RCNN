f896827f8e328c7daabbb5b0f1a736e95b2ec951
Ryan Avery <ravery@ucsb.edu>
2019-06-25 10:41:06 -0700

    fixed bug

--- bash-scripts/setup_env.sh
+++ bash-scripts/setup_env.sh
@@ -1,5 +1,5 @@
 source ~/.bashrc
 conda update -n base -c defaults conda -y
-/data/anaconda/envs/py36/bin/conda env update -f ~/work/$1/cropmask-env.yml -y
+conda env update -n py36 --file ~/work/$1/cropmask-env.yml
 cd ~/work/$1/
 /data/anaconda/envs/py36/bin/python setup.py develop
