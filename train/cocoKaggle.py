4debc4a1450aa543b0e9019501e401afaac63772
Akashdeep Jassal <akash.deep.jassal@hotmail.com>
2019-07-31 13:34:16 +0530

    Changed MObileNetV1 config for Kaggle

--- train/cocoKaggle.py
+++ train/cocoKaggle.py
@@ -480,13 +480,13 @@ if __name__ == '__main__':
         dataset_train = CocoDataset()
         dataset_train.load_coco(args.dataset, "train", year=args.year, auto_download=args.download)
         if args.year in '2014':
-            dataset_train.load_coco('./', "valminusminival", year=args.year, auto_download=args.download)
+            dataset_train.load_coco('', "valminusminival", year=args.year, auto_download=args.download)
         dataset_train.prepare()
 
         # Validation dataset
         dataset_val = CocoDataset()
         val_type = "val" if args.year in '2017' else "minival"
-        dataset_val.load_coco('./', val_type, year=args.year, auto_download=args.download)
+        dataset_val.load_coco('', val_type, year=args.year, auto_download=args.download)
         dataset_val.prepare()
 
         # Image Augmentation
