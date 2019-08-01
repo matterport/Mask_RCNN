770af35479969263b881e0e69bc7105d8beddf82
Akashdeep Jassal <akash.deep.jassal@hotmail.com>
2019-08-01 09:07:46 +0530

    Changed MObileNetV1 config

--- mrcnn/configMobileNetV1.py
+++ mrcnn/configMobileNetV1.py
@@ -124,8 +124,8 @@ class Config(object):
     #         size IMAGE_MIN_DIM x IMAGE_MIN_DIM. Can be used in training only.
     #         IMAGE_MAX_DIM is not used in this mode.
     IMAGE_RESIZE_MODE = "square"
-    IMAGE_MIN_DIM = 1024//2
-    IMAGE_MAX_DIM = 1024//2
+    IMAGE_MIN_DIM = 800
+    IMAGE_MAX_DIM = 1024
     # Minimum scaling ratio. Checked after MIN_IMAGE_DIM and can force further
     # up scaling. For example, if set to 2 then images are scaled up to double
     # the width and height, or more, even if MIN_IMAGE_DIM doesn't require it.
