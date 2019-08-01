e157bdbef7b987d3a401ec3b75e05d980a54b8ab
Akashdeep Jassal <akash.deep.jassal@hotmail.com>
2019-08-01 12:13:56 +0530

    Changed MObileNetV1 config

--- mrcnn/configMobileNetV1.py
+++ mrcnn/configMobileNetV1.py
@@ -31,7 +31,7 @@ class Config(object):
     # handle 2 images of 1024x1024px.
     # Adjust based on your GPU memory and image sizes. Use the highest
     # number that your GPU can handle for best performance.
-    IMAGES_PER_GPU = 4
+    IMAGES_PER_GPU = 1
 
     # Number of training steps per epoch
     # This doesn't need to match the size of the training set. Tensorboard
