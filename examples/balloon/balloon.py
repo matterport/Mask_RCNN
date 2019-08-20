580755cd5b3e910a4d56ffa5c51bb990af8f1920
akashdeepjassal <akashdeepjassal>
2019-08-20 13:12:40 +0530

    Masde Changes to balloon model

--- examples/balloon/balloon.py
+++ examples/balloon/balloon.py
@@ -359,7 +359,10 @@ if __name__ == '__main__':
             "mrcnn_class_logits", "mrcnn_bbox_fc",
             "mrcnn_bbox", "mrcnn_mask"])
     else:
-        model.load_weights(weights_path, by_name=True)
+        #model.load_weights(weights_path, by_name=True)
+        model.load_weights(weights_path, by_name=True, exclude=[
+            "mrcnn_class_logits", "mrcnn_bbox_fc",
+            "mrcnn_bbox", "mrcnn_mask"])
 
     # Train or evaluate
     if args.command == "train":
