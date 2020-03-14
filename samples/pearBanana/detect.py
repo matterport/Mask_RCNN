9b0beeaff85d628a9dd1f61a5ead21f789db9bfe
Wang Chengsheng <c.wang54@student.unimelb.edu.au>
2020-03-14 23:06:46 +1100

    set up samples
    set up samples . no module trained yet
    TODO:test shapes,test ballon

---
+++ samples/pearBanana/detect.py
@@ -0,0 +1,61 @@
+import os
+import sys
+import random
+import math
+import numpy as np
+import skimage.io
+import matplotlib
+import matplotlib.pyplot as plt
+
+# Root directory of the project
+ROOT_DIR = os.path.abspath("../../")
+
+# Import Mask RCNN
+sys.path.append(ROOT_DIR)  # To find local version of the library
+from mrcnn import utils
+import mrcnn.model as modellib
+from mrcnn import visualize
+
+# Import config
+sys.path.append(os.path.join(ROOT_DIR, "samples/pearBanana/"))  # To find local version
+import fruit
+
+# get_ipython().run_line_magic('matplotlib', 'inline')
+
+# Directory to save logs and trained model
+MODEL_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Local path to trained weights file
+COCO_MODEL_PATH = os.path.join(ROOT_DIR, "logs/fruit20200314T1744/mask_rcnn_fruit_0010.h5")
+
+# Directory of images to run detection on
+IMAGE_DIR = os.path.join(ROOT_DIR, "datasets/fruit/test")
+
+class InferenceConfig(fruit.BalloonConfig):
+    # Set batch size to 1 since we'll be running inference on
+    # one image at a time. Batch size = GPU_COUNT * IMAGES_PER_GPU
+    GPU_COUNT = 1
+    IMAGES_PER_GPU = 1
+
+
+config = InferenceConfig()
+config.display()
+
+# Create model object in inference mode.
+model = modellib.MaskRCNN(mode="inference", model_dir=MODEL_DIR, config=config)
+
+# Load weights trained on MS-COCO
+model.load_weights(COCO_MODEL_PATH, by_name=True)
+
+class_names = ['BG', 'banana', 'pear']
+
+file_names = next(os.walk(IMAGE_DIR))[2]
+image = skimage.io.imread(os.path.join(IMAGE_DIR, random.choice(file_names)))
+
+# Run detection
+results = model.detect([image], verbose=1)
+
+# Visualize results
+r = results[0]
+visualize.display_instances(image, r['rois'], r['masks'], r['class_ids'],
+                            class_names, r['scores'])
