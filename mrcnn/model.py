b1feba91636ea8f7e50f35c0820fd370b13586ac
sunghoon-choi <sunghoon.choi@linecorp.com>
2020-04-22 16:36:32 +0900

    Set workers to 1 in train()

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2370,7 +2370,7 @@ class MaskRCNN():
             validation_data=val_generator,
             validation_steps=self.config.VALIDATION_STEPS,
             max_queue_size=100,
-            workers=workers,
+            workers=1,
             use_multiprocessing=False,
         )
         self.epoch = max(self.epoch, epochs)
