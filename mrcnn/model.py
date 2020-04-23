c35b5b1c32b76f9c50c1f76a5e06970c589717b1
sunghoon-choi <sunghoon.choi@linecorp.com>
2020-04-23 10:40:29 +0900

    Enable TensorBoard callback when every epoch ends

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2337,8 +2337,8 @@ class MaskRCNN():
 
         # Callbacks
         callbacks = [
-            # keras.callbacks.TensorBoard(log_dir=self.log_dir,
-            #                             histogram_freq=0, write_graph=True, write_images=False),
+            keras.callbacks.TensorBoard(log_dir=self.log_dir,
+                                        histogram_freq=0, write_graph=True, write_images=False),
             # keras.callbacks.ModelCheckpoint(self.checkpoint_path,
             #                                 verbose=0, save_weights_only=True),
         ]
