3b92d076e1f0334cedd9d43eed168ec8472d7cf8
Rich Hosek <richhosek@gmail.com>
2020-07-27 10:29:16 -0500

    sparse_tensor_to_dense -> sparse.to_dense

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -719,7 +719,7 @@ def refine_detections_graph(rois, probs, deltas, window, config):
         conf_keep = tf.where(class_scores >= config.DETECTION_MIN_CONFIDENCE)[:, 0]
         keep = tf.sets.intersection(tf.expand_dims(keep, 0),
                                         tf.expand_dims(conf_keep, 0))
-        keep = tf.sparse_tensor_to_dense(keep)[0]
+        keep = tf.sparse.to_dense(keep)[0]
 
     # Apply per-class NMS
     # 1. Prepare variables
@@ -757,7 +757,7 @@ def refine_detections_graph(rois, probs, deltas, window, config):
     # 4. Compute intersection between keep and nms_keep
     keep = tf.sets.intersection(tf.expand_dims(keep, 0),
                                     tf.expand_dims(nms_keep, 0))
-    keep = tf.sparse_tensor_to_dense(keep)[0]
+    keep = tf.sparse.to_dense(keep)[0]
     # Keep top detections
     roi_count = config.DETECTION_MAX_INSTANCES
     class_scores_keep = tf.gather(class_scores, keep)
