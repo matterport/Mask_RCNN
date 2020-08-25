2adab81bd66940a8cc40d58bf382eb3a0d46ed2e
Dhaulagiri0 <yiqinw4@gmail.com>
2020-08-25 21:07:34 +0800

    added memory growth

--- angio2020/angio_train.py
+++ angio2020/angio_train.py
@@ -22,6 +22,19 @@ from mrcnn import visualize
 from angiodataset import AngioDataset
 import json
 
+import tensorflow as tf
+
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
 
 """Arrange resutls to match COCO specs in http://cocodataset.org/#format
 """
