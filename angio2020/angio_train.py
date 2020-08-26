a97841dd45c19dd926235bb610745ec49487cfff
Dhaulagiri0 <yiqinw4@gmail.com>
2020-08-26 19:44:30 +0800

    added cov0 layer before conv1

--- angio2020/angio_train.py
+++ angio2020/angio_train.py
@@ -234,10 +234,7 @@ if __name__ == '__main__':
 
 
     print("Loading weights ", weights_path)
-    if mode == 'train':
-        model.load_weights(weights_path, by_name=True, exclude=['conv1'])
-    else:
-        model.load_weights(weights_path, by_name=True)
+    model.load_weights(weights_path, by_name=True)
 
     if mode == 'inference':
         class_names = ['BG', 'artery']
@@ -245,22 +242,20 @@ if __name__ == '__main__':
         # Load a random image from the images folder
         file_names = next(os.walk(IMAGE_DIR))[2]
 
-        mean = 0
-        cnt = 0
-
         for name in file_names:
             if name.split('.')[-1] == 'jpeg':
                 
                 # load image
                 pred_image = skimage.io.imread(os.path.join(IMAGE_DIR, name))
+                image = skimage.color.gray2rgb(pred_image)
 
                 # converts image to rgb if it is grayscale
                 if pred_image.ndim != 3:
-                    image = skimage.color.gray2rgb(pred_image)
+                    pred_image = np.expand_dims(pred_image, -1)
+                else:
+                    pred_image = skimage.color.rgb2gray(pred_image)
                     pred_image = np.expand_dims(pred_image, -1)
                 
-                mean += np.mean(pred_image)
-                cnt += 1
                 # Run detection
                 results = model.detect([pred_image], verbose=1)
                 # Visualize results
