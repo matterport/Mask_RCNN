2cd42b2ba785de9724bfb634a0e0f4f6037953ba
BuildTools <unconfigured@null.spigotmc.org>
2020-08-27 12:01:47 +0800

    changed last stage training to 150 epochs

--- angio2020/angio_train.py
+++ angio2020/angio_train.py
@@ -322,6 +322,6 @@ if __name__ == '__main__':
         print("Fine tune all layers")
         model.train(dataset_train, dataset_val,
                     learning_rate=config.LEARNING_RATE / 10,
-                    epochs=100,
+                    epochs=150,
                     layers='all',
                     augmentation=augmentation)
