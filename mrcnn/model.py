1a30fa22fd02f13d993f714153349c255f519579
fedefedo92@gmail.com <fedefedo92@gmail.com>
2021-03-17 02:06:51 +0100

    mstrics_sensor init in model.py

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -1835,6 +1835,7 @@ class MaskRCNN():
         self.model_dir = model_dir
         self.set_log_dir()
         self.keras_model = self.build(mode=mode, config=config)
+        self.keras_model.metrics_tensors = []
 
     def build(self, mode, config):
         """Build Mask R-CNN architecture.
