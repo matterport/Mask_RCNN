82b194e12858c2e2b770bfb19c5d98c46b8c5c27
fedefedo92@gmail.com <fedefedo92@gmail.com>
2021-07-02 14:38:03 +0200

    added histograms for tensoboard

--- mrcnn/model.py
+++ mrcnn/model.py
@@ -2339,7 +2339,7 @@ class MaskRCNN():
         # Callbacks
         callbacks = [
             keras.callbacks.TensorBoard(log_dir=self.log_dir,
-                                        histogram_freq=1, write_graph=True, write_images=False),
+                                        histogram_freq=1.0, write_graph=True, write_images=False),
             keras.callbacks.ModelCheckpoint(self.checkpoint_path,
                                             verbose=0, save_weights_only=True),
         ]
