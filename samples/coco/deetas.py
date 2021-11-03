2d59347c2431dcf82fdf635458a0fb64919582c1
jhmang1128 <jhmang1128@gmail.com>
2021-11-03 15:45:34 +0900

    update_customizing

--- samples/coco/deetas.py
+++ samples/coco/deetas.py
@@ -50,6 +50,7 @@ import shutil
 
 # Root directory of the project
 ROOT_DIR = os.path.abspath("../")
+CURRENT_DIR = os.path.abspath("./")
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
@@ -61,7 +62,7 @@ COCO_MODEL_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
 
 # Directory to save logs and model checkpoints, if not provided
 # through the command line argument --logs
-DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+DEFAULT_LOGS_DIR = os.path.join(CURRENT_DIR, "output_maeng/logs")
 DEFAULT_DATASET_YEAR = ""
 
 ############################################################
